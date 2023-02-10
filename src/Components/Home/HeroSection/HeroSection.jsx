ddfdd440c821a27a5bef4d215c41cc8ac4d02ce7
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-02-10 17:43:30 +0600

    resp: hero done

--- src/Components/Home/HeroSection/HeroSection.jsx
+++ src/Components/Home/HeroSection/HeroSection.jsx
@@ -6,11 +6,11 @@ import Typewriter from 'typewriter-effect';
 const HeroSection = () => {
   
   return (
-    <div className={`pt-16 px-10 pb-10 h-screen flex justify-center items-center`}>
-      <div>
-        <h1 className={`font-poppins font-semibold text-[#1f98df] text-2xl my-4`}>Assalamualaikum. I am</h1>
-        <h1 className={`font-poppins font-extrabold text-[#1f98df] leading-snug text-4xl my-4`}>Mohammed <span className={`text-[#eeb90b]`}>Adib</span></h1>
-        <h2 className={` font-poppins font-semibold leading-snug text-4xl my-4 text-[#eeb90b]`}>
+    <div className={`pt-16 px-10 pb-10 font-poppins font-semibold text-[#1f98df] leading-snug text-4xl sm:text-5xl lg:text-7xl h-screen flex justify-center sm:justify-start items-center`}>
+      <div className={`sm:ml-10 md:ml-14 lg:ml-20`}>
+        <h1 className={`text-2xl sm:text-3xl my-4`}>Assalamualaikum. I am</h1>
+        <h1 className={`font-extrabold my-4`}>Mohammed <span className={`text-[#eeb90b]`}>Adib</span></h1>
+        <h2 className={`my-4 text-[#eeb90b] text-2xl sm:text-3xl md:text-4xl`}>
           My motto is, 	<br />
         <Typewriter 
           options={{
