21a5185e0fa00b2d39db1faaf00a79dde980b7d0
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-02-18 20:46:53 +0600

    experience icon updated

--- src/Components/Home/Experience/Experience.jsx
+++ src/Components/Home/Experience/Experience.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-
+import { CgBriefcase, CgReadme, CgSmileMouthOpen } from "react-icons/cg";
 
 const Experience = () => {
      return (
@@ -14,7 +14,7 @@ const Experience = () => {
                          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                          date="2011 - present"
                          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                         // icon={<WorkIcon />}
+                         icon={<CgBriefcase />}
                     >
                          <h3 className="vertical-timeline-element-title">Creative Director</h3>
                          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
@@ -24,9 +24,11 @@ const Experience = () => {
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                          className="vertical-timeline-element--work"
+                         contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                         contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                          date="2010 - 2011"
                          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                         // icon={<WorkIcon />}
+                         icon={<CgBriefcase />}
                     >
                          <h3 className="vertical-timeline-element-title">Art Director</h3>
                          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
@@ -36,9 +38,11 @@ const Experience = () => {
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                          className="vertical-timeline-element--work"
+                         contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                         contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                          date="2008 - 2010"
                          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                         // icon={<WorkIcon />}
+                         icon={<CgBriefcase />}
                     >
                          <h3 className="vertical-timeline-element-title">Web Designer</h3>
                          <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
@@ -48,9 +52,11 @@ const Experience = () => {
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                          className="vertical-timeline-element--work"
+                         contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                         contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                          date="2006 - 2008"
                          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                         // icon={<WorkIcon />}
+                         icon={<CgBriefcase />}
                     >
                          <h3 className="vertical-timeline-element-title">Web Designer</h3>
                          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
@@ -60,9 +66,11 @@ const Experience = () => {
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                          className="vertical-timeline-element--education"
+                         contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                         contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                          date="April 2013"
                          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                         // icon={<SchoolIcon />}
+                         icon={<CgReadme />}
                     >
                          <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
                          <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
@@ -72,9 +80,11 @@ const Experience = () => {
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                          className="vertical-timeline-element--education"
+                         contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                         contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                          date="November 2012"
                          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                         // icon={<SchoolIcon />}
+                         icon={<CgReadme />}
                     >
                          <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
                          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
@@ -84,9 +94,11 @@ const Experience = () => {
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                          className="vertical-timeline-element--education"
+                         contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                         contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                          date="2002 - 2006"
                          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                         // icon={<SchoolIcon />}
+                         icon={<CgReadme />}
                     >
                          <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
                          <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
@@ -96,7 +108,7 @@ const Experience = () => {
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                          iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-                         // icon={<StarIcon />}
+                         icon={<CgSmileMouthOpen />}
                     />
                </VerticalTimeline>
           </div>
