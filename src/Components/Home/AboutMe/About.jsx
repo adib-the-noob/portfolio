8f41d1ddf3c7aeac1f35efcd7c73350b72a1a1e6
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-02-18 23:08:06 +0600

    about updated

--- src/Components/Home/AboutMe/About.jsx
+++ src/Components/Home/AboutMe/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
      <h1 className={`text-center text-4xl md:text-5xl lg:text-6xl text-[#eeb90b] pb-16 md:pb-20 lg:pb-24`}>About Me</h1>
      <div className={`grid grid-cols-1 md:grid-cols-2 justify-items-center content-center px-10 md:px-16 lg:px-20 gap-y-10`}>
           <img src={adib} alt='Mohammed Adib' className={`w-32 md:w-40 lg:w-56 h-32 md:h-40 lg:h-56 rounded-full ring-[5px] ring-[#1f98df] block my-auto hover:shadow-2xl  hover:shadow-[#1f98df] transition-all duration-700`}/>
-          <p className={`font-light text-[#c5c4c1c7] text-justify lg:text-xl my-auto`}>I am ADIB-the-NOOB, a tech enthusiast with a passion for coding. My GitHub showcases diverse projects in web development and data analysis. I am dedicated to continuously improving and expanding my skillset through learning new programming languages. My passion for technology drives me to produce high-quality projects.</p>
+          <p className={`font-light text-[#c5c4c1c7] text-justify lg:text-xl my-auto`}>Hello, my name is Adib and I am a backend developer from Bangladesh with a passion for creating dynamic and efficient backend applications. I specialize in working with Django and the Django Rest Framework, and I am always eager to learn new technologies and improve my skills to deliver the best possible solutions for my clients. I am an experienced developer with a strong background in creating Backend applications that are both functional and visually appealing. Connect with me to know more about my skills and experience.</p>
      </div>
     </div>
   )
